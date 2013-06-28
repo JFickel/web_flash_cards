5bf77b95fbc818e521108fa994c407c2d87c024d
Apprentice <apprentice@dbc01.local>
2013-06-28 12:27:02 -0700

    Added session controller logic

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,10 +1,17 @@
 get '/sessions/new' do
-  erb :'sessions/new'
+  erb :'/sessions/new'
 end
 
 post '/sessions' do
-  erb :'decks/index'
+  if user = User.authenticate(params[:email],params[:password])
+    session[:user_id] = user.id
+    erb :'decks/index'
+  else
+    erb :'/sessions/new'
+  end
 end
 
 delete '/sessions' do
+  session.clear
+  redirect '/'
 end
