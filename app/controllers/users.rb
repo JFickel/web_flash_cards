d8515d4b27050cf47487a4456aa1145e3612d7fc
dbc-apprentice <dbc@dbc.com>
2013-06-28 15:24:27 -0700

    Added save/error logic to post /users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,8 +6,17 @@ get '/users/new' do
   erb :'/users/new'
 end
 
+
 post '/users' do
-  erb :'/decks/index' #if successful
-  erb :'/users/new' #if unsuccessful
+  user = User.new(email: params[:email], password: params[:password])
+  if user.save
+    session[:user_id] =  user.id
+    session.delete(:signup_errors)
+    erb :'/decks/index'
+     #if successful
+  else
+    session[:signup_errors] = user.errors
+    erb :'/users/new' #if unsuccessful
+  end
 end
 
